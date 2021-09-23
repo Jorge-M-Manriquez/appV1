511863fcfe05903ef14d6910fcad0ccf3bb73366
Jorge-M-Manriquez <jorg.montecinos@duocuc.cl>
2021-09-22 22:36:00 -0300

    pag 2 añadida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'pag2',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'pag2',
+    loadChildren: () => import('./pages/pag2/pag2.module').then( m => m.Pag2PageModule)
   },
 ];
 
